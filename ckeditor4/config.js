5ae839ad5476bc61116a90765405a194d564f23a
sallecta <sallecta@inbox.ru>
2023-07-24 00:56:23 +0300

    v1.0

---
+++ ckeditor4/config.js
@@ -0,0 +1,18 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'tools' },
+		{ name: 'others' },
+		{ name: 'about' }
+	];
+	config.extraPlugins = 'btns';
+};
---
+++ start_ckeditor.js
@@ -0,0 +1,15 @@
+﻿CKEDITOR.config.height = 150;
+CKEDITOR.config.width = 'auto';
+
+var start_ckeditor = ( function() {
+	return function() {
+		var editorElement = CKEDITOR.document.getById( 'editor' );
+		if ( CKEDITOR.plugins.get( 'wysiwygarea' ) ) {
+			CKEDITOR.replace( 'editor' );
+		} else {
+			editorElement.setAttribute( 'contenteditable', 'true' );
+			CKEDITOR.inline( 'editor' );
+		}
+	};
+} )();
+
